3c43c6d40f47ca40695b04b8afa0e7f84316f6f4
Yukito Ito <yukibukiyou@gmail.com>
2021-04-03 12:34:24 +0900

    f

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ try {
   const command = core.getInput('prettier_command');
   const targetFiles = core.getInput('target_files')
 
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
   const prettierCommand = `${targetFiles} | xargs ${command}`
 
   console.log(`${prettierCommand}`)
   console.log(execSync(prettierCommand).toString())
 
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
