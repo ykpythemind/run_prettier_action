10f6fe16409eaddbe83e7a0913c88cdd99e59f75
Yukito Ito <yukibukiyou@gmail.com>
2021-04-03 12:54:23 +0900

    fi

--- dist/index.js
+++ dist/index.js
@@ -12,14 +12,13 @@ const { execSync } = __nccwpck_require__(129);
 
 try {
   const command = core.getInput('prettier_command');
-  const baseBranch = core.getInput('base_branch')
 
-  console.log('basebranch')
-  console.log(baseBranch)
-
-  if (baseBranch) {
-    console.log(execSync(`git fetch ${baseBranch} --depth 1`).toString())
-  }
+  // console.log('basebranch')
+  // console.log(baseBranch)
+  //
+  // if (baseBranch) {
+  //   console.log(execSync(`git fetch ${baseBranch} --depth 1`).toString())
+  // }
 
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
