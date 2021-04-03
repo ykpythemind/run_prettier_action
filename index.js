5f8044735204656c1d7d058b481e51218fbb255d
Yukito Ito <yukibukiyou@gmail.com>
2021-04-03 12:51:57 +0900

    fi

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ const { execSync } = require('child_process');
 
 try {
   const command = core.getInput('prettier_command');
-  const baseBranch = core.getInput('base_branch')
 
-  console.log('basebranch')
-  console.log(baseBranch)
-
-  if (baseBranch) {
-    console.log(execSync(`git fetch ${baseBranch} --depth 1`).toString())
-  }
+  // console.log('basebranch')
+  // console.log(baseBranch)
+  //
+  // if (baseBranch) {
+  //   console.log(execSync(`git fetch ${baseBranch} --depth 1`).toString())
+  // }
 
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
