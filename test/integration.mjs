ab3e551bd184f4352dd35dff533f13a1acb3d09f
ykpythemind <yukibukiyou@gmail.com>
2021-12-28 16:21:46 +0900

    test

--- test/integration.mjs
+++ test/integration.mjs
@@ -33,107 +33,108 @@ if (!remoteBranchName) {
   throw Error("specify REMOTE_BRANCH");
 }
 
+const dir = process.env.DIR;
+if (!dir) {
+  throw Error("specify DIR");
+}
+
 const branchName = generateRandomString(32);
 
 (async () => {
-  mkdtemp(path.join(os.tmpdir(), "foo-"), async (err, directory) => {
-    if (err) throw err;
+  process.chdir(dir);
 
-    process.chdir(directory);
+  // execSync(`git config --global user.email "test@example.com"`);
+  // execSync(`git config --global user.name "ykpythemind"`);
 
-    execSync(`git config --global user.email "test@example.com"`);
-    execSync(`git config --global user.name "ykpythemind"`);
+  // const stdout = execSync(
+  //   "git clone https://github.com/ykpythemind/run_prettier_action ."
+  // );
 
-    const stdout = execSync(
-      "git clone https://github.com/ykpythemind/run_prettier_action ."
-    );
+  execSync(`git checkout -b ${branchName} origin/${remoteBranchName}`);
 
-    execSync(`git checkout -b ${branchName} origin/${remoteBranchName}`);
+  execSync(`echo 'const a    = "fuga"' > dummy.js`);
+  console.log(execSync("ls -la").toString());
 
-    execSync(`echo 'const a    = "fuga"' > dummy.js`);
-    console.log(execSync("ls -la").toString());
+  execSync(`git add .`);
+  execSync(`git commit -m 'test'`);
 
-    execSync(`git add .`);
-    execSync(`git commit -m 'test'`);
+  let pr = null;
+  const owner = "ykpythemind";
+  const repo = "run_prettier_action";
 
-    let pr = null;
-    const owner = "ykpythemind";
-    const repo = "run_prettier_action";
+  try {
+    execSync(`git push origin -u ${branchName}`);
 
-    try {
-      execSync(`git push origin -u ${branchName}`);
+    const resp = await octokit.pulls.create({
+      owner,
+      repo,
+      head: branchName,
+      base: "main",
+      title: `[test] ${branchName}`,
+    });
 
-      const resp = await octokit.pulls.create({
-        owner,
-        repo,
-        head: branchName,
-        base: "main",
-        title: `[test] ${branchName}`,
-      });
+    console.log(resp);
+    pr = resp.data.number;
 
-      console.log(resp);
-      pr = resp.data.number;
+    const commits = await octokit.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: pr,
+    });
+    console.log(commits);
+    const commitsnum = commits.data.length;
 
+    const comment = await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: pr,
+      body: "/p",
+    });
+
+    console.log("sleep...");
+    await sleep(30);
+
+    for (let item of Array(30)) {
       const commits = await octokit.pulls.listCommits({
         owner,
         repo,
         pull_number: pr,
       });
-      console.log(commits);
-      const commitsnum = commits.data.length;
+      const newCommitNum = commits.data.length;
 
-      const comment = await octokit.issues.createComment({
-        owner,
-        repo,
-        issue_number: pr,
-        body: "/p",
-      });
+      console.log(`c ${commitsnum}: c2: ${newCommitNum}`);
+      if (commitsnum < newCommitNum) {
+        console.log(commits.data);
+        console.log("found!!!!!!!");
 
-      console.log("sleep...");
-      await sleep(30);
-
-      for (let item of Array(30)) {
-        const commits = await octokit.pulls.listCommits({
+        const lastCommit = commits.data.at(-1);
+        if (lastCommit.commit.message === "Apply prettier changes") {
+          console.log("found the commit!", lastCommit.commit);
+        } else {
+          throw Error("commit is not expected", lastCommit);
+        }
+        break;
+      }
+      await sleep(2);
+    }
+  } catch (e) {
+    console.error(e);
+  } finally {
+    if (pr) {
+      withErrorHandling(async () => {
+        // if pr, close...
+        await octokit.pulls.update({
           owner,
           repo,
           pull_number: pr,
+          state: "closed",
         });
-        const newCommitNum = commits.data.length;
-
-        console.log(`c ${commitsnum}: c2: ${newCommitNum}`);
-        if (commitsnum < newCommitNum) {
-          console.log(commits.data);
-          console.log("found!!!!!!!");
-
-          const lastCommit = commits.data.at(-1);
-          if (lastCommit.commit.message === "Apply prettier changes") {
-            console.log("found the commit!", lastCommit.commit);
-          } else {
-            throw Error("commit is not expected", lastCommit);
-          }
-          break;
-        }
-        await sleep(2);
-      }
-    } catch (e) {
-      console.error(e);
-    } finally {
-      if (pr) {
-        withErrorHandling(async () => {
-          // if pr, close...
-          await octokit.pulls.update({
-            owner,
-            repo,
-            pull_number: pr,
-            state: "closed",
-          });
-          console.log("[cleanup] closed pull request");
-        });
-        withErrorHandling(async () => {
-          execSync(`git push origin --delete ${branchName}`);
-          console.log("[cleanup] delete remote temporary branch");
-        });
-      }
+        console.log("[cleanup] closed pull request");
+      });
+      withErrorHandling(async () => {
+        execSync(`git push origin --delete ${branchName}`);
+        console.log("[cleanup] delete remote temporary branch");
+      });
     }
-  });
+  }
 })();
